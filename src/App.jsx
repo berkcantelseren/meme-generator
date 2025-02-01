2c0df0310627e317180197ba94d643ac9b5bca37
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-01 22:22:35 +0300

    main and header info add + troll-face image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
-
-export default App;
