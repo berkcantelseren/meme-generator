4bbb811530b5efdea29550051a8f059792138924
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-02 17:13:51 +0300

    useEffect add for api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Main() {
   const [meme, setMeme] = useState({
@@ -6,6 +7,14 @@ export default function Main() {
     bottomText: "Walk into Mordor",
     imageUrl: "http://i.imgflip.com/1bij.jpg",
   });
+
+  const [getMeme, setGetMeme] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setGetMeme(data.data.memes));
+  }, []);
 
   function handleChange(event) {
     const { value, name } = event.currentTarget;
